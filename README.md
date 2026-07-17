de35122791f17f5ea5028b4db9498ba10632adf3
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-07-17 16:55:38 +0300

    Refactor

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 ```shell
 docker run --rm -it --init --read-only \
-  --user "$(id -u):$(id -g)" \
+  -e PUID="$(id -u)" \
+  -e PGID="$(id -g)" \
   --tmpfs /tmp \
-  -v ~/.yii-pi:/pi \
+  -v yii-pi-sessions:/pi-sessions \
+  -v "$HOME"/.pi/agent/models.json:/pi/models.json:ro \
   -v "$(pwd)":/workspace \
   ghcr.io/yiisoft-contrib/pi-harness:latest
 ```
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,32 +1,14 @@
 #!/bin/sh
 set -eu
 
-cp /pi-yii/SYSTEM.md /pi/SYSTEM.md
+PUID="${PUID:-0}"
+PGID="${PGID:-0}"
+if [ "$PUID" = 0 ] && [ "$PGID" = 0 ]; then
+  exec "$@"
+fi
 
-PI_EXTENSIONS="/pi-yii/extensions"
-PI_PROMPTS="/pi-yii/prompts"
-PI_SETTINGS="/pi/settings.json"
-node -e "
-    const fs = require('fs');
-    let settings;
-    try {
-        settings = JSON.parse(fs.readFileSync('$PI_SETTINGS', 'utf8'));
-    } catch (e) {
-        settings = {};
-    }
-    if (!Array.isArray(settings.extensions)) {
-        settings.extensions = [];
-    }
-    if (!settings.extensions.includes('$PI_EXTENSIONS')) {
-        settings.extensions.push('$PI_EXTENSIONS');
-    }
-    if (!Array.isArray(settings.prompts)) {
-        settings.prompts = [];
-    }
-    if (!settings.prompts.includes('$PI_PROMPTS')) {
-        settings.prompts.push('$PI_PROMPTS');
-    }
-    fs.writeFileSync('$PI_SETTINGS', JSON.stringify(settings, null, 2) + '\n');
-"
+chown -R "$PUID:$PGID" "$PI_CODING_AGENT_SESSION_DIR"
 
-exec "$@"
+export HOME=/tmp/user-home
+
+exec setpriv --reuid="$PUID" --regid="$PGID" --clear-groups "$@"
