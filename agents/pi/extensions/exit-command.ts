baea780680fb907c87d885048a4c056d59a6ebaf
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-10 10:22:26 +0300

    improve /exit in pi

--- agents/pi/extensions/exit-command.ts
+++ agents/pi/extensions/exit-command.ts
@@ -2,13 +2,15 @@
  * Exit Command Extension
  *
  * Adds a /exit command that allows cleanly shutting down pi.
+ *
+ * @see https://github.com/earendil-works/pi/issues/5863
  */
 
 import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
 
 export default function (pi: ExtensionAPI) {
 	pi.registerCommand("exit", {
-		description: "Exit pi",
+		description: "Exit Pi (alias for /quit)",
 		handler: async (_args, ctx) => {
 			ctx.shutdown();
 		},
